3837e57a7b36f49946dfc6a9c51ffed2ed3e3998
madush123 <ranasin-ec21037@stu.kln.ac.lk>
2025-11-10 19:02:15 +0530

    .

--- src/main/java/com/ev_booking_system/api/model/User.java
+++ src/main/java/com/ev_booking_system/api/model/User.java
@@ -1,10 +1,11 @@
-package com.evhub.backend.model;
+package com.ev_booking_system.api.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 public class User {
+
     @Id
     private String id;
     private String name;
@@ -12,7 +13,8 @@ public class User {
     private String password;
     private String role; // "SUPERADMIN", "OPERATOR", or "USER"
 
-    public User() {}
+    public User() {
+    }
 
     public User(String name, String email, String password, String role) {
         this.name = name;
