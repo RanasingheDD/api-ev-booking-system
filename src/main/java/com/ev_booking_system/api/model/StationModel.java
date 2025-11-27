4f4bb0a8d5323a836e5c518d2ac99e14790e4e04
madush123 <ranasin-ec21037@stu.kln.ac.lk>
2025-11-27 15:34:32 +0530

    Station Model update

--- src/main/java/com/ev_booking_system/api/model/StationModel.java
+++ src/main/java/com/ev_booking_system/api/model/StationModel.java
@@ -1,13 +1,17 @@
 package com.ev_booking_system.api.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Document(collection = "ev_stations")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class StationModel{
+public class StationModel {
+
     private String id;
     private String owner_name;
     private long mobile;
