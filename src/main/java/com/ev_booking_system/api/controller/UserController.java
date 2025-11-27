f4b24b1fc1f23189aeab2c34bdeb80f3b6fcff02
Madushan Chathuranghe <72508591+madush123@users.noreply.github.com>
2025-11-27 19:48:56 +0530

    Refactor UserController for better response handling
    Updated user registration and login methods to return ResponseEntity with appropriate HTTP status codes. Added email existence check during registration.

--- src/main/java/com/ev_booking_system/api/controller/UserController.java
+++ src/main/java/com/ev_booking_system/api/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ev_booking_system.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import com.ev_booking_system.api.dto.LoginRequest;
 import com.ev_booking_system.api.dto.UserDto;
 import com.ev_booking_system.api.model.EvModel;
@@ -9,9 +10,13 @@ import com.ev_booking_system.api.model.UserModel;
 import com.ev_booking_system.api.service.UserService;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.ev_booking_system.api.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api/users")
@@ -20,10 +25,18 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/register")
-    public UserModel registerUser(@RequestBody UserModel user) {
-        return userService.registerUser(user);
+    public ResponseEntity<?> registerUser(@RequestBody UserModel user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already exists");
+        }
+
+        user.setRole("USER");
+        userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
     }
 
     @GetMapping
@@ -32,15 +45,23 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public UserDto loginUser(@RequestBody LoginRequest loginRequest) {
-    return userService.loginUser(loginRequest.getEmail(), loginRequest.getPassword());
+    public ResponseEntity<UserDto> loginUser(@RequestBody LoginRequest loginRequest) {
+        UserDto user = userService.loginUser(loginRequest.getEmail(), loginRequest.getPassword());
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
     @PostMapping("/addEv")
-    public EvModel addEV(@RequestBody EvModel evModel) { 
+    public EvModel addEV(@RequestBody EvModel evModel) {
         return userService.addEV(evModel);
     }
-    
 
-    
+    @PutMapping("/{email}")
+    public UserDto updateUser(@PathVariable String email, @RequestBody UserDto updatedUser) {
+        return userService.updateUser(email, updatedUser);
+    }
+
 }
