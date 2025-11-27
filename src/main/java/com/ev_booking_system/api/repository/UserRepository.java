54cd98a2cc68e9c3e144c89d4a44724de3f5676a
Madushan Chathuranghe <72508591+madush123@users.noreply.github.com>
2025-11-27 19:54:26 +0530

    Add email existence check and findByEmail method

--- src/main/java/com/ev_booking_system/api/repository/UserRepository.java
+++ src/main/java/com/ev_booking_system/api/repository/UserRepository.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import com.ev_booking_system.api.model.UserModel;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends MongoRepository<UserModel, String> {
+
     UserModel findByEmail(String email);
-    UserModel findByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
