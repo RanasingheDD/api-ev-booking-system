df77f35bb499cbaf3ea505e29dc05d61987d5731
RanasingheDD <dishand398@gmail.com>
2025-12-14 11:54:08 +0000

    update-ev and station reposity

--- src/main/java/com/ev_booking_system/api/repository/EvRepository.java
+++ src/main/java/com/ev_booking_system/api/repository/EvRepository.java
@@ -1,5 +1,7 @@
 package com.ev_booking_system.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import com.ev_booking_system.api.model.EvModel;
 
 @Repository
 public interface EvRepository extends MongoRepository<EvModel, String> {
-    EvModel findByRegistrationNo(String registrationNo);
+    List<EvModel> findByUserId(String userId); 
+    //EvDto findById(String id);
 }
