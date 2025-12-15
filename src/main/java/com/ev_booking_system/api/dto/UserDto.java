e3fc2be49c907d2fb06ba20cd136968533f9dbb5
madush123 <ranasin-ec21037@stu.kln.ac.lk>
2025-12-15 16:55:31 +0530

    UserDto to improve code formatting and readability

--- src/main/java/com/ev_booking_system/api/dto/UserDto.java
+++ src/main/java/com/ev_booking_system/api/dto/UserDto.java
@@ -1,17 +1,18 @@
 package com.ev_booking_system.api.dto;
 
 import java.util.List;
+
 import com.ev_booking_system.api.model.Role;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
+
     private String token;
     private String id;
     private String username;
